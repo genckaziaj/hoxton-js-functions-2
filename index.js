dcb16f30dcd973da26398f3d1afc36c3d6b2e978
Genci23 <genckaziaj@yahoo.com>
2022-06-20 20:20:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+Description
+Let's practice extracting data with map, filter. And sprinkle some timer magic, too.
+
+Instructions
+- Use this starting template, you'll find the data there => https://codesandbox.io/s/day-6-functions-template-es4eg?file=/data.js
+- As always, do not code your solution in CodeSandbox
+Using the usual Users and Todo lists:
+  - Prompt the user for a letter
+  - Find all the users who's name contains the letter 
+  - Say hi to those users in the console - but only 1 greeting every 2 seconds.
+  - Prompt the user for a user ID. Using the value, find all the todos that are Incomplete and console.log them.
+
+Tips
+- Don't forget to clear interval once it's done running
+- It's perfectly okay to prompt a user for details while other stuff (i.e.the interval) is running
+- Keep your code separated into functions for easier readability later
+
+Challenge
+- Prompt the user to enter a property (e.g. 'email' or 'address') and then console.log a list of name - given property. E.g. for email it would look along the lines of: 
+
+Bob Marley - bob@marley.com
+Steven Hawking - hawk@this.co.uk
+*/
